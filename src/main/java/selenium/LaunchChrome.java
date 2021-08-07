5a9c5de16d65729244799e0395b5d88b6f87f198
Ananya Sharma <49600702+ananya1-de@users.noreply.github.com>
2021-08-07 10:57:55 +0530

    update URL

--- src/main/java/selenium/LaunchChrome.java
+++ src/main/java/selenium/LaunchChrome.java
@@ -8,7 +8,7 @@ public class LaunchChrome {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\shana\\Downloads\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.google.com");
+		driver.get("http://admin.emoha.com");
 		System.out.println(driver.getTitle());
 
 	}
